9e85f612fdb4b4daad42610069a68669012876aa
Aditya Kumar <60425987+adityaxtalk@users.noreply.github.com>
2023-05-02 18:28:35 +0530

    movie app added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
+import React,{useState, useEffect, useCallback} from 'react';
 import './App.css';
+import MovieList from './components/MovieList';
+import AddMovie from './components/AddMovie';
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading]= useState(false);
+  const [error, setError] = useState(false);
+   const fetchMoviesHandler = useCallback(async ()=>{
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response= await fetch('https://my-data-api-8a0f5-default-rtdb.firebaseio.com/movies.json');
+        if (!response.ok) {
+          throw new Error('Something went wrong!');
+        }
+
+        const data=await response.json();
+        const loadedMovies=[];
+        for (const key in data) {
+          loadedMovies.push({
+            id: key,
+            title: data[key].title,
+            openingText: data[key].openingText,
+            releaseDate: data[key].releaseDate
+          });
+        }
+        const transformedMovies= loadedMovies.map((movieData)=>{
+          return {
+            id: movieData.id,
+            title: movieData.title,
+            openingText: movieData.openingText,
+            releaseDate: movieData.releaseDate
+          }
+        });
+        setMovies(transformedMovies);
+      } catch(error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+   }, []);
+
+   useEffect(()=>{
+    fetchMoviesHandler();
+   }, [fetchMoviesHandler]);
+   
+   async function addMovieHandler(movie) {
+    const response= await fetch('https://my-data-api-8a0f5-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data=await response.json();
+    console.log(data);
+   }
+
+   let content=<p>Found no movies.</p>
+   if (movies.length>0) {
+    content=<MovieList movies={movies} />
+   }
+
+   if (error) {
+    content=<p>{error}</p>
+   }
+
+   if (isLoading) {
+    content='...loading'
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <section>
+      <AddMovie onAddMovie={addMovieHandler} />
+    </section>
+    <section>
+      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+    </section>
+    <section>{content}</section>
+    </>
   );
 }
 
